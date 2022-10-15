e7d899f140088150e4456129582920eff82286ff
Polux021 <65615269+Poluxin21@users.noreply.github.com>
2022-10-14 22:01:18 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// CREDITS: Polux ( Please do not remove )
-// THE SOFTWARE WILL BE UPDATED CONSTANTLY. WITH MORE FEATURES AND OPTIMIZATIONS SO STAY TUNED :)
-
-const { Main } = require("./functions")
-
-Main("usr", "pass", "text") // MODIFY
